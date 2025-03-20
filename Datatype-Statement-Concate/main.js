2a26186f0b38ae0ee10daccf2f61cc3d617087bf
farid <farid01729366777@gmail.com>
2025-03-20 21:26:01 +0600

    datatype statement implement

---
+++ Datatype-Statement-Concate/main.js
@@ -0,0 +1,9 @@
+
+
+['vat', 'tehari', 'kacchi', 'borhani', 'rooti']; //This is the data of array 
+
+ "my name is khan"; "u also chan";
+
+console.log("hello chan banu, how are you"+ "are u okay chan banu?" +"where u gone now");
+ 
+ 
