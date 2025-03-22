40cfbf3709eae07d99225b883557391a96c38e27
farid <farid01729366777@gmail.com>
2025-03-22 22:04:00 +0600

    end commit var-let-const-template-literal-operator

---
+++ var-let-const-template-literal-operator/index.js
@@ -0,0 +1,46 @@
+
+var userName;
+  userName = 'chan-mia';
+  age = 50;
+// var userName = prompt('userName');
+// var age = prompt('age');
+userName = "abdul latif"
+userName = "abdul kuddus amr nam"
+userName = "kadir mia amr nam"
+age=" amr age 60"
+console.log("Hello " + userName + ",   age was" + age + "years");
+
+let name;
+name = 'asraful hoque'
+// let name='hello' error show cause scope issue 
+profession='React native developer'
+console.log("Hello everyone ami holam " + name + ", ami holam " + profession + "");
+
+
+let names = 'Jueab';
+age = 40;
+skill = "Mern Developer";
+// console.log("Mr "+ +", he was "+ +", and he was a "+ +" ");=> this is the one pattern
+// console.log(`Mr ${names},he was-${age},and he was a-${skill}`);=>Modern pattern
+console.log(
+  `
+  Names:${names}-
+  Age:${age}-
+  Skills:${skill}j
+  `
+  
+)
+ 
+/** 
+ * @Operators 
+ * 
+*/
+        
+/**
+ * @+= er kaz
+ */
+let anyName = 'Hello Manush';
+anyName += ', How are you today'; //(+= er kaz holo previouse value er shate presen value add kroe dewa thats all);
+console.log(anyName);
+ 
+
